6d52ac3c9afb995ae2b00c357aa29308d925bb6a
xkuadrattt <calmystu.00@gmail.com>
2024-09-26 18:46:56 +0700

    fixing variable workPeriod method

--- src/utils/workPeriod.ts
+++ src/utils/workPeriod.ts
@@ -10,10 +10,6 @@ export const workPeriod = (hireDate: string) => {
   const today = new Date();
   const years = differenceInYears(today, start);
   const months = differenceInMonths(today, start) % 12;
-  const days = differenceInDays(
-    today,
-    new Date(today.getFullYear(), today.getMonth() - 1, start.getDate())
-  );
 
   return `${years}y ${months}mo`;
 };
